35f223c813d4ddf2a0673b2368deeb262c0219cf
mh <mh@immerda.ch>
2008-07-27 20:27:25 +0000

    added rpm notice

--- manifests/init.pp
+++ manifests/init.pp
@@ -45,6 +45,9 @@ class squid::gentoo inherits squid::base {
     }
 }
 
+# rpms: we got the latest rpm from here:
+# http://people.redhat.com/mnagy/squid/
+# distro rpms seems to have problems...
 class squid::centos inherits squid::base {
     file{'/etc/sysconfig/squid':
         source => [ "puppet://$server/files/squid/sysconfig/${fqdn}/squid",
