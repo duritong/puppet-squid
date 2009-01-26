f4292c6d27817d451988694796c213a19b831000
mh <mh@immerda.ch>
2009-01-26 22:20:46 +0000

    using the correct munin define

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -5,5 +5,5 @@ class squid::munin {
     munin::plugin{ 'squid_icp': }
     munin::plugin{ 'squid_requests': }
     munin::plugin{ 'squid_traffic': }
-    munin::remoteplugin{ 'squid_efficiency': source => "puppet://$server/squid/munin/squid_efficiency" }
+    munin::plugin::deploy{ 'squid_efficiency': source => "puppet://$server/squid/munin/squid_efficiency" }
 }
