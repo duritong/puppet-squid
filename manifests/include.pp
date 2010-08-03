0f6fd13a31746fbe29fe87a82260cd8e76d4b244
mh <mh@immerda.ch>
2010-08-03 23:11:06 +0200

    introduce a way to pass content for includes

--- manifests/include.pp
+++ manifests/include.pp
@@ -1,11 +1,20 @@
-define squid::include(){
+define squid::include($content = undef){
   include ::squid
   require squid::includedir
 
   file{"/etc/squid/include.d/${name}":
-    source => [ "puppet:///modules/site-squid/include.d/${fqdn}/${name}",
-                "puppet:///modules/site-squid/include.d/${name}" ],
     notify => Service['squid'],
     owner => root, group => squid, mode => 0640;
+  }
+
+  if !$content {
+    File["/etc/squid/include.d/${name}"]{
+      source => [ "puppet:///modules/site-squid/include.d/${fqdn}/${name}",
+                  "puppet:///modules/site-squid/include.d/${name}" ]
+    }
+  } else {
+    File["/etc/squid/include.d/${name}"]{
+      content => $content,
+    }
   }
 }
