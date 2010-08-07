ff9a2668178e6e90c228dbcfccfb881ea9be4752
mh <mh@immerda.ch>
2010-08-07 02:12:27 +0200

    remove unnecessary fileserver variable (#2460)

--- manifests/base.pp
+++ manifests/base.pp
@@ -13,9 +13,9 @@ class squid::base {
     file {"squid_config":
         path => "/etc/squid/squid.conf",
         ensure => file, owner => root, group => root, mode => 644,
-        source => [ "puppet://$server/modules/site-squid/${fqdn}/squid.conf",
-                    "puppet://$server/modules/site-squid/squid.conf",
-                    "puppet://$server/modules/squid/squid.conf" ],
+        source => [ "puppet:///modules/site-squid/${fqdn}/squid.conf",
+                    "puppet:///modules/site-squid/squid.conf",
+                    "puppet:///modules/squid/squid.conf" ],
         notify => Service[squid],
     }
 }
--- manifests/centos.pp
+++ manifests/centos.pp
@@ -3,9 +3,9 @@
 # distro rpms seems to have problems...
 class squid::centos inherits squid::base {
     file{'/etc/sysconfig/squid':
-        source => [ "puppet://$server/modules/site-squid/sysconfig/${fqdn}/squid",
-                    "puppet://$server/modules/site-squid/sysconfig/squid",
-                    "puppet://$server/modules/squid/sysconfig/squid" ],
+        source => [ "puppet:///modules/site-squid/sysconfig/${fqdn}/squid",
+                    "puppet:///modules/site-squid/sysconfig/squid",
+                    "puppet:///modules/squid/sysconfig/squid" ],
         require => Package['squid'],
         notify => Service['squid'],
         owner => root, group => 0, mode => '0644';
