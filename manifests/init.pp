40bed26dda08c26518798f21e6df1fb6f0e8c561
o <o@immerda.ch>
2012-06-14 10:31:14 -0300

    remove hiera

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,14 +2,16 @@
 # Copyright (C) 2007 admin@immerda.ch
 #
 
-class squid {
+class squid (
+  $manage_munin = false
+) {
   case $::operatingsystem {
     gentoo: { include squid::gentoo }
     centos: { include squid::centos }
     default: { include squid::base }
   }
 
-  if hiera('use_munin',false) {
+  if $squid::manage_munin {
     include squid::munin
   }
 }
